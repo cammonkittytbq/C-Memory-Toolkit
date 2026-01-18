756156f759b54c0a120320e849d5b5130852e9f5
m-rossi-tech <princeton0xusaci00@gmail.com>
2026-01-18 16:19:37 +0700

    Update allocator.c

--- allocator.c
+++ allocator.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
-    int *p = malloc(sizeof(int));
-    if(p) {
-        *p = 42;
-        printf("Memory check: %d\n", *p);
-        free(p);
+#include <string.h>
+
+// Custom memory allocation utility
+void check_memory() {
+    size_t size = 1024;
+    char *buffer = (char *)malloc(size);
+
+    if (buffer != NULL) {
+        strcpy(buffer, "Memory status: OK");
+        printf("%s\n", buffer);
+        free(buffer);
+    } else {
+        printf("Allocation failed!\n");
     }
+}
+
+int main() {
+    printf("Initializing Memory Toolkit...\n");
+    check_memory();
     return 0;
 }
